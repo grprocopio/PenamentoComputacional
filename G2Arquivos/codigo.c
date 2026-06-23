ec30e03519354cc623216c984557c820f09e76f4
Guilherme Rossato Procopio <guilherme.rprocopio@hotmail.com>
2026-06-23 20:23:32 -0300

    Update and rename g2.md.c to codigo.c

--- G2Arquivos/codigo.c
+++ G2Arquivos/codigo.c
@@ -1,8 +1,6 @@
 https://docs.google.com/document/d/1_SlcHYBJQVeAbvrkxkNduAIEecrZ-06KLPzVps7wGCM/edit?usp=sharing
 
 
-<img width="720" height="1280" alt="image" src="https://github.com/user-attachments/assets/4f8934f4-9373-47f1-8da4-d8fb3f94b2ec" />
-
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <UniversalTelegramBot.h>
